990fa51310cb1b9974e100b0a9c622405a841d4d
sahil9001 <sahilsilare@gmail.com>
2020-04-21 18:09:39 +0530

    Changes to be committed: 	modified:   bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,19 @@ for i in xrange(no_of_friends):
     name = str(raw_input("Name: ")) 
     friends = client.searchForUsers(name)  # return a list of names 
     friend = friends[0] 
-    msg = str(raw_input("Message: ")) 
-    lyrics = lyricwikia.get_lyrics('Eminem', 'Mockingbird')
-    lyrics = lyrics.split(" ")
-    for word in lyrics:
-   	    sent = client.sendMessage(word, thread_id=friend.uid) 
-    if sent: 
-        print("Message sent successfully!") 
-
+    print(friends)
+    case = int(raw_input("1 for Lyrics, 2 for personal message:"))
+    if case == 1:
+        lyrics = lyricwikia.get_lyrics('Eminem', 'Mockingbird')
+        lyrics = lyrics.split(" ")
+        for word in lyrics:
+            sent = client.sendMessage(word, thread_id=friend.uid) 
+        if sent: 
+            print("Message sent successfully!") 
+    else:
+        msg = str(raw_input("Message: ")) 
+        n = int(raw_input("No. of times: "))
+        for i in range(0,n):
+            sent = client.sendMessage(msg, thread_id=friend.uid) 
+        if sent:
+            print("Message sent successfully")    
